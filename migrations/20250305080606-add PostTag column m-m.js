2c8b68d1cea503a4158f3ab8987632cd76784afc
hafidzahm <hafizhahm123@gmail.com>
2025-03-05 15:13:53 +0700

    CHORE: init db

---
+++ migrations/20250305080606-add PostTag column m-m.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('PostTags', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      }
+    })
+    await queryInterface.addColumn('PostTags', 'TagId', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Tags',
+        id: 'id'
+      }
+    })
+    await queryInterface.addColumn('PostTags', 'PostId', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Posts',
+        id: 'id'
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    await queryInterface.removeColumn('PostTags', 'PostId')
+    await queryInterface.removeColumn('PostTags', 'TagId')
+    await queryInterface.dropTable('PostTags')
+  }
+};
